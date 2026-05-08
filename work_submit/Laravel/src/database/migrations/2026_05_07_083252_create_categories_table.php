21dcbd1a3e98a2a54520d8fae60d04ba283dcf6a
Yuka Kashihara <yuka.kashihara.carecon@gmail.com>
2026-05-08 17:40:28 +0900

    add:migrationを追加

--- work_submit/Laravel/src/database/migrations/2026_05_07_083252_create_categories_table.php
+++ work_submit/Laravel/src/database/migrations/2026_05_07_083252_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('last_name');
-            $table->string('first_name');
-            $table->string('last_name_kana');
-            $table->string('first_name_kana');
-            $table->string('email')->unique();
-            $table->string('password_hash');
-            $table->integer('status');
+            $table->string('name');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -34,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 }
